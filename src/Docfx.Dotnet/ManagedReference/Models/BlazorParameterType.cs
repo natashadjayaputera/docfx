7a2e964aabdfab72f10b3f15b988ab73752617bd
Natasha Djayaputera <natashadjayaputera@gmail.com>
2024-09-17 13:37:46 +0700

    Added BlazorParameterType

---
+++ src/Docfx.Dotnet/ManagedReference/Models/BlazorParameterType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Docfx.DataContracts.ManagedReference;
+public enum BlazorParameterType
+{
+    CascadingParameter,
+    Parameter,
+    None
+}
